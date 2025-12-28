713fcbba4523f87cbb64d10d9ee6adbd6e3e5398
RaschePat <kms0317mc@naver.com>
2025-12-28 17:04:08 +0900

    댓글창 구현

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,42 +3,39 @@ import Slider from "react-slick";
 import Loading from "../../components/common/Loading";
 import { useGetFeeds } from "../../queries/postQueries";
 import * as s from "./styles";
-
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useEffect, useRef, useState } from "react";
-import { PulseLoader } from "react-spinners";
+import { FadeLoader } from "react-spinners";
 import { IoMdHeart, IoMdHeartEmpty } from "react-icons/io";
 import { IoChatbubbleOutline } from "react-icons/io5";
+import Comment from "../../components/comment/Comment";
 
-function Home(){
-    const [ commentOpen, setCommentOpen ] = useState(false);
-    const { isLoading, isFetching, isPending, data, hasNextPage, fetchNextPage } = useGetFeeds();
+function Home() {
+    const [ commentOpen, setCommentOpen ] = useState(0);
+    const { isLoading, isFetching, data, hasNextPage, fetchNextPage } = useGetFeeds();
     const loadMoreRef = useRef();
-    console.log(isFetching);
-    console.log(isLoading);
-    console.log(isPending);
 
     useEffect(() => {
         const observer = new IntersectionObserver((entries) => {
             const [obs] = entries;
-            if(obs.isIntersecting && hasNextPage){
+            if (obs.isIntersecting && hasNextPage) {
                 fetchNextPage();
             }
-        },{threshold: 1});
-        
-        observer.observe(loadMoreRef.current);
-    },[hasNextPage]);
+        }, {threshold: 1});
 
-    const handleCommentOnclick = () => {
-        setCommentOpen(!commentOpen);
+        observer.observe(loadMoreRef.current);
+    }, [hasNextPage]);
+
+    const handleCommentOnClick = (postId) => {
+        setCommentOpen(commentOpen === postId ? 0 : postId);
     }
 
     return <div css={s.layout}>
         <div css={s.feedContainer(commentOpen)}>
             {
                 (isLoading && <Loading />) || data.pages.map(feeds => feeds.data.contents.map(feed => (
-                    <div key={feed.feedId} css={s.feedItemContainer}>
+                    <div key={feed.postId} css={s.feedItemContainer}>
                         <header>
                             <div css={s.profileImage(feed.user?.imgUrl)}></div>
                             <div css={s.userInfo}>
@@ -48,17 +45,17 @@ function Home(){
                         </header>
                         <main>
                             {
-                                feed.imageFiles &&
+                                feed.imageFiles && 
                                 <div css={s.feedImageContainer}>
                                     <Slider
-                                        infinite= {true}
-                                        speed= {500}
-                                        slidesToShow= {1}
-                                        slidesToScroll= {1}>
+                                        infinite={true}
+                                        speed={500}
+                                        slidesToShow={1}
+                                        slidesToScroll={1}>
                                             {
-                                                feed.imageFiles.map( file => (
-                                                <div css={s.feedImage("http://localhost:8080/image"+file.filePath)}>
-                                                </div>
+                                                feed.imageFiles.map(file => (
+                                                    <div css={s.feedImage("http://localhost:8080/image" + file.filePath)}>
+                                                    </div>
                                                 ))
                                             }
                                     </Slider>
@@ -69,30 +66,27 @@ function Home(){
                             </div>
                         </main>
                         <footer>
-                            <div>{false ? <IoMdHeart/> : <IoMdHeartEmpty/> }</div>
-                            <div onClick={handleCommentOnclick}><IoChatbubbleOutline/></div>
+                            <div>{false ? <IoMdHeart /> : <IoMdHeartEmpty />}</div>
+                            <div onClick={() => handleCommentOnClick(feed.postId)}><IoChatbubbleOutline /></div>
                         </footer>
                     </div>
                 )))
             }
-            <div ref={loadMoreRef}>
+            <div ref={loadMoreRef} style={{padding: "10px 0"}}>
                 {
-                    isFetching && !isLoading &&
-                    <PulseLoader
-                        color="rgba(32, 32, 32, 0.93)"
-                        loading
-                        margin={4}
-                        size={8}
-                        speedMultiplier={0.7}/>
+                    isFetching && !isLoading && <FadeLoader />
                 }
             </div>
         </div>
         <div css={s.commentContainer(commentOpen)}>
+            {
+                !!commentOpen &&
+                <Comment postId={commentOpen} />
+            }
+        </div>
+        <div css={s.followInfoContainer}>
 
         </div>
-        {/* <div css={s.followInfoContainer} style={{padding:"10px 0"}}> 
-
-        </div> */}
     </div>
 }
 
